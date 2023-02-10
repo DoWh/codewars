42d4d1d91daadecb1808bb5d0eabe73168ac0521
DoWh <dinkudom@gmail.com>
2023-02-10 21:43:42 +0300

    cakes

---
+++ cakes.js
@@ -0,0 +1,19 @@
+function cakes(recipe, available) {
+    let count,current;
+    for(ingredient in recipe){
+        // console.log(available[ingredient] != undefined);
+        if (available[ingredient] != undefined) {
+            current = Math.trunc(available[ingredient] / recipe[ingredient]);
+            if ((current < count) || (count == undefined)) count = current;
+        } else return 0;
+    }
+    return count;
+}
+
+let recipe = {flour: 500, sugar: 200, eggs: 1};
+let available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200};
+
+// let recipe = {apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100};
+// let available = {sugar: 500, flour: 2000, milk: 2000};
+
+console.log(cakes(recipe, available))
