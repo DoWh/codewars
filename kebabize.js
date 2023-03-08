9d584685ee57f9868b428ecd11f979541fbc7738
DoWh <dinkudom@gmail.com>
2023-03-08 19:37:39 +0300

    kebabize

---
+++ kebabize.js
@@ -0,0 +1,14 @@
+function kebabize(str) {
+    console.log(str)
+    return str
+    //delete all exept a-z, A-Z
+    .replace(/[^a-zA-Z]/g , '')
+    //replace all A-Z to small and add '-'
+    .replace(/[A-Z]/g, (match)=>{
+        return '-' + match.toLowerCase();
+    })
+    //replace first '-', if word start with A-Z
+    .replace(/^-/, '')
+}
+
+console.log(kebabize("-myCamelHas3Humps"))
